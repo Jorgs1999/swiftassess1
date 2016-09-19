8bc7305b2934656ae858a18985fa6ec123a496a3
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2016-09-19 07:54:10 -0400

    Initial Commit

---
+++ Swift assess 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift assess 1
+//
+//  Created by BHSRam4 on 9/19/16.
+//  Copyright © 2016 BHSRam4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
