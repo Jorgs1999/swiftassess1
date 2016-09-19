d5f578df24b4201d6afc95cdf7a5ecbedf265869
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2016-09-19 08:10:18 -0400

    first commit

--- Swift assess 1/ViewController.swift
+++ Swift assess 1/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel2: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+         self.view.backgroundColor = UIColor.yellow    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
